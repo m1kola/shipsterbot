3ce0af6d1b73828efc72ec90a940ae50b748f16a
Mikalai Radchuk <radchuk.m@gmail.com>
2017-11-21 22:10:57 +0000

    Describe schema for the PostgreSQL DB

---
+++ schema.sql
@@ -0,0 +1,16 @@
+create table unfinished_commands (
+	id serial primary key,
+	command varchar(32) not null,
+	chat_id int not null,
+	created_by int not null,
+	created_at timestamp not null,
+	unique (chat_id, created_by)
+);
+
+create table shopping_items (
+	id serial primary key,
+	name varchar(255) not null,
+	chat_id int not null,
+	created_by int not null,
+	created_at timestamp not null
+);
