638e9dea9fa0755bfc5ff0005a036e2b56da2096
Mikalai Radchuk <radchuk.m@gmail.com>
2017-12-16 23:05:04 +0000

    Add few simple tests

--- env/common.go
+++ env/common.go
@@ -2,10 +2,12 @@ package env
 
 import "os"
 
+// GetDBConnectionString returns a string representing DB connection
 func GetDBConnectionString() string {
 	return os.Getenv("DATABASE_URL")
 }
 
+// IsDebug returns a bool indicating current execution mode
 func IsDebug() bool {
 	return os.Getenv("DEBUG") == "true"
 }
