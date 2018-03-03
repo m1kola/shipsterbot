12a400329054717f5604c1cffdf3413a4d7a3c44
Mikalai Radchuk <radchuk.m@gmail.com>
2018-03-03 12:25:17 +0000

    Adds tests for handleUpdate
    Cover error handeling

--- bot/telegram/update_handlers_test.go
+++ bot/telegram/update_handlers_test.go
@@ -109,6 +109,102 @@ func TestHandleUpdate(t *testing.T) {
 		}
 	})
 
+	t.Run("CallbackQuery update: handlerCanNotHandleError", func(t *testing.T) {
+		// Data mocks
+		messageMock := &tgbotapi.Message{}
+		callbackQueryMock := &tgbotapi.CallbackQuery{
+			Message: messageMock,
+		}
+		updateMock := tgbotapi.Update{
+			CallbackQuery: callbackQueryMock,
+		}
+
+		// Function mocks
+		handleCallbackQueryIsCalled := false
+		handleCallbackQueryOld := handleCallbackQuery
+		defer func() { handleCallbackQuery = handleCallbackQueryOld }()
+		handleCallbackQuery = func(
+			_ botClientInterface,
+			_ storage.DataStorageInterface,
+			callbackQuery *tgbotapi.CallbackQuery,
+		) error {
+			handleCallbackQueryIsCalled = true
+			if callbackQueryMock != callbackQuery {
+				t.Error("Wrong CallbackQuery received")
+			}
+
+			return handlerCanNotHandleError{errors.New("Fake error")}
+		}
+
+		handleUnrecognisedMessageIsCalled := false
+		handleUnrecognisedMessageOld := handleUnrecognisedMessage
+		defer func() { handleUnrecognisedMessage = handleUnrecognisedMessageOld }()
+		handleUnrecognisedMessage = func(_ sender, message *tgbotapi.Message) {
+			handleUnrecognisedMessageIsCalled = true
+			if message != messageMock {
+				t.Error("Wrong Message received")
+			}
+		}
+
+		handleUpdate(clientMock, stMock, updateMock)
+
+		if !handleCallbackQueryIsCalled {
+			t.Error("func handleCallbackQuery wasn't called")
+		}
+		if !handleUnrecognisedMessageIsCalled {
+			t.Error("func handleUnrecognisedMessage wasn't called")
+		}
+
+	})
+
+	t.Run("Message update: handlerCanNotHandleError", func(t *testing.T) {
+		// Data mocks
+		messageMock := &tgbotapi.Message{
+			Chat: &tgbotapi.Chat{
+				ID: 123,
+			},
+		}
+		updateMock := tgbotapi.Update{
+			Message: messageMock,
+		}
+
+		// Function mocks
+		handleMessageIsCalled := false
+		handleMessageOld := handleMessage
+		defer func() { handleMessage = handleMessageOld }()
+		handleMessage = func(
+			_ sender,
+			_ storage.DataStorageInterface,
+			message *tgbotapi.Message,
+		) error {
+			handleMessageIsCalled = true
+			if message != messageMock {
+				t.Error("Wrong Message received")
+			}
+
+			return handlerCanNotHandleError{errors.New("Fake error")}
+		}
+
+		handleUnrecognisedMessageIsCalled := false
+		handleUnrecognisedMessageOld := handleUnrecognisedMessage
+		defer func() { handleUnrecognisedMessage = handleUnrecognisedMessageOld }()
+		handleUnrecognisedMessage = func(_ sender, message *tgbotapi.Message) {
+			handleUnrecognisedMessageIsCalled = true
+			if message != messageMock {
+				t.Error("Wrong Message received")
+			}
+		}
+
+		handleUpdate(clientMock, stMock, updateMock)
+
+		if !handleMessageIsCalled {
+			t.Error("func handleCallbackQuery wasn't called")
+		}
+		if !handleUnrecognisedMessageIsCalled {
+			t.Error("func handleUnrecognisedMessage wasn't called")
+		}
+	})
+
 	t.Run("CallbackQuery update error", func(t *testing.T) {
 		// Data mocks
 		messageMock := &tgbotapi.Message{
