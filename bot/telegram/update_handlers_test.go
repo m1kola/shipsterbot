a722411df9323d3415ef802573845a58fd1317d5
Mikalai Radchuk <radchuk.m@gmail.com>
2018-03-10 11:12:05 +0000

    Adds tests for handleList

--- bot/telegram/update_handlers_test.go
+++ bot/telegram/update_handlers_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/m1kola/shipsterbot/gomockhelpers"
 	"github.com/m1kola/shipsterbot/mocks/bot/mock_telegram"
 	"github.com/m1kola/shipsterbot/mocks/mock_storage"
+	"github.com/m1kola/shipsterbot/models"
 	"github.com/m1kola/shipsterbot/storage"
 )
 
@@ -251,6 +252,106 @@ func TestHandleAdd(t *testing.T) {
 			}))
 
 			err := handleAdd(clientMock, stMock, messageMock)
+
+			if err != nil {
+				t.Errorf("Unexpected err: got %#v", err)
+			}
+		})
+	})
+}
+
+func TestHandleList(t *testing.T) {
+	// Common interface mocks
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	clientMock := mock_telegram.NewMocksender(mockCtrl)
+	stMock := mock_storage.NewMockDataStorageInterface(mockCtrl)
+
+	// Common data mocks
+	errMock := errors.New("fake error")
+	messageMock := &tgbotapi.Message{
+		Chat: &tgbotapi.Chat{
+			ID: 123,
+		},
+		From: &tgbotapi.User{
+			ID: 321,
+		},
+	}
+
+	t.Run("Storage error", func(t *testing.T) {
+		stMock.EXPECT().GetShoppingItems(messageMock.Chat.ID).Return(nil, errMock)
+
+		err := handleList(clientMock, stMock, messageMock)
+
+		if !strings.Contains(err.Error(), errMock.Error()) {
+			t.Errorf("Expected err %#v, got %#v", errMock, err)
+		}
+	})
+
+	t.Run("Success", func(t *testing.T) {
+		t.Run("Empty shopping list", func(t *testing.T) {
+			// Data mocks
+			storageDataMock := []*models.ShoppingItem{}
+
+			stMock.EXPECT().GetShoppingItems(gomock.Any()).Return(storageDataMock, nil)
+			clientMock.EXPECT().Send(gomockhelpers.MatcherFunc(func(x interface{}) bool {
+				msgCfg, ok := x.(tgbotapi.MessageConfig)
+				if !ok {
+					return false
+				}
+
+				// Reply to the same chat
+				if msgCfg.ChatID != messageMock.Chat.ID {
+					return false
+				}
+
+				if !strings.Contains(msgCfg.Text, "list is empty") {
+					return false
+				}
+
+				return true
+			}))
+
+			err := handleList(clientMock, stMock, messageMock)
+
+			if err != nil {
+				t.Errorf("Unexpected err: got %#v", err)
+			}
+		})
+
+		t.Run("Shopping list with items", func(t *testing.T) {
+			// Data mocks
+			storageDataMock := []*models.ShoppingItem{
+				&models.ShoppingItem{
+					Name: "Milk",
+				},
+				&models.ShoppingItem{
+					Name: "Молоко",
+				},
+			}
+
+			stMock.EXPECT().GetShoppingItems(gomock.Any()).Return(storageDataMock, nil)
+			clientMock.EXPECT().Send(gomockhelpers.MatcherFunc(func(x interface{}) bool {
+				msgCfg, ok := x.(tgbotapi.MessageConfig)
+				if !ok {
+					return false
+				}
+
+				// Reply to the same chat
+				if msgCfg.ChatID != messageMock.Chat.ID {
+					return false
+				}
+
+				for _, dataItem := range storageDataMock {
+					if !strings.Contains(msgCfg.Text, dataItem.Name) {
+						return false
+					}
+				}
+
+				return true
+			}))
+
+			err := handleList(clientMock, stMock, messageMock)
 
 			if err != nil {
 				t.Errorf("Unexpected err: got %#v", err)
